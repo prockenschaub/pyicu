8fa75aca714d07495add6dd45aa439f7839de5f5
prockenschaub <rockenschaub.patrick@gmail.com>
2022-11-03 19:05:28 +0100

    Add getters to ID config

--- src/pyicu/configs/id.py
+++ src/pyicu/configs/id.py
@@ -1,27 +1,55 @@
-from typing import Type, Tuple, Dict
+from typing import Type, Dict
 import pandas as pd
 from .utils import check_attributes_in_dict
 
 class IdCfg():
     def __init__(self, cfg: pd.DataFrame = None):
-        self.cfg = cfg
+        self.cfg = cfg   # TODO: change to a better name
         self.cfg.sort_values('position', inplace=True)
 
     def from_dict(x: Dict) -> Type['IdCfg']:
         """_summary_
         """
-        attr = ['id', 'position', 'start', 'end', 'table']
+        attr = ['name', 'id', 'position', 'start', 'end', 'table']
 
         ids = []
         for name, cfg in x.items():
             cfg = cfg.copy()
-            cfg['id'] = name
+            cfg['name'] = name
             check_attributes_in_dict(cfg, attr, name, 'id')
             ids += [cfg]
 
         cfgs = pd.DataFrame(ids, columns=attr)
+        cfgs.set_index('position', inplace=True)
         return IdCfg(cfgs)
             
+    def __getitem__(self, id: str) -> pd.Series:
+        if not isinstance(id, str):
+            raise TypeError(f'Expected an ID type (e.g., icustay) as string, got {id.__class__}.')
+        if not any(self.cfg.name == id):
+            raise ValueError(f'ID type {id} not defined.')
+        return self.cfg[self.cfg.name == id].squeeze()
+
+    @property
+    def names(self) -> pd.Series:
+        return self.cfg.name
+
+    @property
+    def ids(self) -> pd.Series:
+        return self.cfg.id
+
+    @property
+    def starts(self) -> pd.Series:
+        return self.cfg.start
+
+    @property
+    def ends(self) -> pd.Series:
+        return self.cfg.end
+
+    @property
+    def tables(self) -> pd.Series:
+        return self.cfg.table
+
     def __repr__(self) -> str:
         repr = ""
         for id in self.cfg.id:
@@ -29,4 +57,4 @@ class IdCfg():
                 repr += " < "
             repr += id
         
-        return f"<IdCfg: [{repr}]>"
+        return f"<IdCfg>: [{repr}]\n{self.cfg.__repr__()}"
