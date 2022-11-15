69a20abc6578187f14eeda9e971d5d4da67d9896
prockenschaub <rockenschaub.patrick@gmail.com>
2022-11-15 09:44:25 +0100

    add more tests

--- tests/test_concept_dict.py
+++ tests/test_concept_dict.py
@@ -1,16 +1,30 @@
 import pytest
 
+from pyicu.concepts.load import config_paths
 from pyicu.concepts import ConceptDict, Concept
 from pyicu.concepts.load import read_dictionary, combine_concepts
 
+@pytest.fixture
+def unparsed_sofa_concept():
+    return {"sofa": read_dictionary()['sofa']}
+
+@pytest.fixture
+def unparsed_hr_concept():
+    return {"hr": read_dictionary()['hr']}
+
+def test_read_dictionary_cfg_dirs():
+    dict0 = read_dictionary()
+    dict1 = read_dictionary(cfg_dirs=config_paths()[0])
+    dict2 = read_dictionary(cfg_dirs=config_paths())
+    assert dict0 == dict1
+    assert dict1 == dict2
 
 def test_loading_default_dict(default_dict):
     raw = read_dictionary()
     assert [r for r in raw.keys()] == [p for p in default_dict.concepts.keys()]
     assert all([isinstance(p, Concept) for p in default_dict.concepts.values()])
 
-def test_loading_two_dicts():
-    default = {"hr": read_dictionary()['hr']}
+def test_loading_two_dicts(unparsed_hr_concept):
     alt = {
         'hr': {
             'sources': {
@@ -19,9 +33,29 @@ def test_loading_two_dicts():
                     'table': 'chartevents',
                     'sub_var': 'itemid'}
         ]}}}
-    fin = combine_concepts(default, alt)
+    fin = combine_concepts(unparsed_hr_concept, alt)
     assert fin['hr']['sources']['mimic'][0]["ids"] == 42
-    
+
+def test_loading_two_dicts_no_overlap(unparsed_hr_concept, unparsed_sofa_concept):
+    combine_concepts(unparsed_sofa_concept, unparsed_hr_concept)
+
+def test_loading_two_dicts_rec(unparsed_sofa_concept):
+    alt = {'sofa': {'sources': {}}}
+    with pytest.raises(ValueError) as e_info:
+        combine_concepts(unparsed_sofa_concept, alt)
+    assert e_info.match("cannot merge recursive")
+
+def test_loading_two_dicts_malformed(unparsed_hr_concept):
+    alt = {'hr': {'sources': [1]}}
+    with pytest.raises(ValueError) as e_info:
+        combine_concepts(unparsed_hr_concept, alt)
+    assert e_info.match("cannot merge .* due to malformed")
+
+def test_loading_two_dicts_non_sources(unparsed_hr_concept):
+    alt = {'hr': {'max': 5000, 'sources': {}}}
+    with pytest.raises(ValueError) as e_info:
+        combine_concepts(unparsed_hr_concept, alt)
+    assert e_info.match("cannot merge .* due to non-`sources`")
 
 def test_getting_single_concept_from_dict(default_dict):
     assert default_dict["hr"] == default_dict.concepts["hr"]
