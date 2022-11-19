ab63212fb0a4071546125ee5a8799e98cedb8d42
prockenschaub <rockenschaub.patrick@gmail.com>
2022-11-19 09:21:24 +0100

    define changing intervals for td

--- src/pyicu/interval.py
+++ src/pyicu/interval.py
@@ -15,3 +15,7 @@ def mins(x):
 
 def secs(x):
     return pd.Timedelta(x, "s")
+
+
+def change_interval(x: pd.Timedelta, new_freq: int, new_unit: str = "h"):
+    return (x // new_freq).floor(new_unit) * new_freq
