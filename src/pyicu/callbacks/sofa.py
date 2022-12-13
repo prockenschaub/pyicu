5e72a0cd12ec7743c06a755e38aec2e73e38890d
prockenschaub <rockenschaub.patrick@gmail.com>
2022-12-13 20:53:44 +0100

    add sofa_single callback

---
+++ src/pyicu/callbacks/sofa.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from .misc import collect_concepts
+
+
+def sofa_single(cnc, nme, fun):
+    def score(x, interval, **kwargs):
+        dat = collect_concepts(x, cnc, interval, **kwargs)
+        dat[nme] = fun(dat[cnc])
+        dat = dat.drop(columns=cnc)
+        return dat
+    return score
+
+sofa_coag = sofa_single("plt", "sofa_coag", lambda x: 4 - pd.cut(x, [-np.inf, 20, 50, 100, 150, np.inf], labels=False))
+
+def findInterval(x, **kwargs):
+    return x
