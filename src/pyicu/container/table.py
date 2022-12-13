8597b0c928ef4f3c6cbcf9eb0ee9ea788173afea
prockenschaub <rockenschaub.patrick@gmail.com>
2022-12-13 20:54:38 +0100

    document and test table accessor

--- src/pyicu/container/table.py
+++ src/pyicu/container/table.py
@@ -1,7 +1,7 @@
 import warnings
 import pandas as pd
 from typing import List, Dict
-from pandas.api.types import is_numeric_dtype, is_timedelta64_dtype, is_bool_dtype, is_string_dtype, is_categorical_dtype
+from pandas.api.types import is_numeric_dtype, is_timedelta64_dtype, is_datetime64_any_dtype, is_bool_dtype, is_string_dtype, is_categorical_dtype
 
 
 from ..utils import enlist, new_names, print_list
@@ -12,10 +12,21 @@ from .unit import UnitDtype
 
 @pd.api.extensions.register_dataframe_accessor("tbl")
 class TableAccessor:
+    """Decorator for pandas DataFrames that adds additional functionality for dealing with ICU tables
+    """
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
 
     def _validate(self):
+        """Check that pandas object fits the requirements of an ICU table
+
+        ICU tables must have a named index. Currently, one, two, or three index levels are supported:
+            - a table with one index corresponds to ricu's id_tbl and the index will be interpreted as an observation identifier
+            - a table with two indices corresponds to ricu's ts_tbl. The second level will be interpreted as a time index. 
+            - a table with three indices corresponds to ricu's win_tbl. The third level will be interpreted as a duration index.  
+        Index levels two and three must be of TimeDtype to ensure they have a corresponding interval frequency recorded. More 
+        than three levels are not currently supported. 
+        """
         # TODO: change to useful error messages that account for the fact that
         #       some methods are available always (e.g., is_id_tbl or change_interval)
         #       and some are only available for id_tbl or ts_tbl
@@ -32,81 +43,152 @@ class TableAccessor:
                     raise AttributeError("if there are two index levels, the second must be a time index (ts_tbl)")
             elif len(levels) == 3:
                 if not (isinstance(levels[-2].dtype, TimeDtype) and isinstance(levels[-1].dtype, TimeDtype)):
-                    raise AttributeError("if there are two index levels, the second must be a time index (win_tbl)")
+                    raise AttributeError("if there are three index levels, the second and third must be a time index (win_tbl)")
             else:
-                raise AttributeError("only MultiIndices with 2 or 3 levels are supported")
+                raise AttributeError("only MultiIndices with two or three levels are supported")
         elif isinstance(obj.index.dtype, TimeDtype):
             raise AttributeError("must have at least one non-time index")
 
     def is_id_tbl(self) -> bool:
+        """Check if the underlying object is an Id table"""
         try:
             self._validate()
             return not isinstance(self._obj.index, pd.MultiIndex)
         except AttributeError:
             return False
 
-    def as_id_tbl(self, id_var: str | None = None):
+    def as_id_tbl(self, id_var: str | None = None) -> pd.DataFrame:
+        """Modify a DataFrame to conform to Id table structure
+        
+        Id tables have a single, named, non-time index such as "icustay_id". 
+
+        Args: 
+            id_var: name of the column that should be set as id variable. If None and no id variable has
+                been set yet, the first non-time column is chosen as id variable. Defaults to None. 
+
+        Return: 
+            pandas object as Id table
+        """
         new_obj = self._obj
-        if self.is_id_tbl():
-            if id_var is None or self.id_var == id_var: 
-                return new_obj
-        elif self.is_ts_tbl():
+        if new_obj.tbl.is_ts_tbl():
             new_obj = new_obj.reset_index(level=1)
-        elif id_var is None:
-            raise NotImplementedError() # Add logic
+        if new_obj.tbl.is_id_tbl():
+            if id_var is None or new_obj.tbl.id_var == id_var: 
+                return new_obj
+        if id_var is None:
+            for c in new_obj.columns:
+                col_type = new_obj[c].dtype
+                if not isinstance(col_type, TimeDtype) and not is_timedelta64_dtype(col_type) and not is_datetime64_any_dtype(col_type):
+                    id_var = c
+                    break
+            if id_var is None: 
+                raise TypeError(f'tried to set id variable automatically but no suitable non-time column could be found')
         return new_obj.tbl.set_id_var(id_var)
 
     def is_ts_tbl(self) -> bool:
+        """Check if the underlying object is a Ts table"""
         try:
             self._validate()
-            return isinstance(self._obj.index, pd.MultiIndex)
+            return isinstance(self._obj.index, pd.MultiIndex) and len(self._obj.index.levels) == 2
         except AttributeError:
             return False
 
-    def as_ts_tbl(self, id_var: str | None = None, index_var: str | None = None):
+    def as_ts_tbl(self, id_var: str | None = None, index_var: str | None = None) -> pd.DataFrame:
+        """Modify a DataFrame to conform to Ts table structure
+        
+        TS tables have a named two-level index where the second level is a time index. 
+        For example, "icustay_id" and "charttime". 
+
+        Args: 
+            id_var: name of the column that should be set as id variable. If None and no id variable has
+                been set yet, the first non-time column is chosen as id variable. Defaults to None. 
+            index_var: name of the column that should be set as time index. If None and no index variable
+                has been setyet, the first time column is chosen as index variable. Defaults to None. 
+
+        Return: 
+            pandas object as Ts table
+        """
         new_obj = self._obj
-        if self.is_id_tbl() and not (id_var is None or self.id_var == id_var):
-            new_obj = new_obj.set_id_var(id_var)
-        elif self.is_ts_tbl():
-            if self.id_var is not None and self.id_var != id_var:
+        if new_obj.tbl.is_id_tbl() and not (id_var is None or new_obj.tbl.id_var == id_var):
+            new_obj = new_obj.tbl.set_id_var(id_var)
+        elif new_obj.tbl.is_ts_tbl():
+            if id_var is not None and new_obj.tbl.id_var != id_var:
                 new_obj = new_obj.tbl.set_id_var(id_var)
-            if self.index_var is None or self.index_var == index_var:
+            if index_var is None or new_obj.tbl.index_var == index_var:
                 return new_obj
-            else: 
-                raise NotImplementedError()
         else: 
-            new_obj = self.as_id_tbl(id_var)
+            new_obj = new_obj.tbl.as_id_tbl(id_var)
             
         if index_var is None: 
-            raise NotImplementedError()
+            for c in new_obj.columns:
+                col_type = new_obj[c].dtype
+                if isinstance(col_type, TimeDtype):
+                    index_var = c
+                    break
+            if index_var is None: 
+                raise TypeError(f'tried to set index variable automatically but no suitable time column could be found')
         return new_obj.tbl.set_index_var(index_var)
 
     @property
     def id_var(self) -> str:
         return self._obj.index.names[0]
 
-    def set_id_var(self, id_var: str, inplace: bool = False) -> pd.DataFrame:
+    def set_id_var(self, id_var: str, drop: bool = False, inplace: bool = False) -> pd.DataFrame:
+        """Set an id for the table
+
+        Args:
+            id_var: name of the column that should be used as time index
+            inplace: _description_. Defaults to False.
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            _description_
+        """
         if not id_var in self._obj.columns:
             if id_var in self._obj.index.names:
                 warnings.warn(f"{id_var} is already part of the metadata, left unchanged")
+                return self._obj
             else: 
                 raise ValueError(f"tried to set Id to unknown column {id_var}")
         new_obj = self._obj
+        old_names = []
         if None not in self._obj.index.names:
-            new_obj = self._obj.reset_index(inplace=inplace)
-        return new_obj.set_index(id_var, drop=True, inplace=inplace)
+            old_names = list(new_obj.index.names)
+            new_obj = self._obj.reset_index(drop=drop, inplace=inplace)
+        return new_obj.set_index([id_var]+old_names[1:], drop=True, inplace=inplace)
 
     @property
     def index_var(self) -> str:
+        """The name of the time index"""
         if self.is_id_tbl():
             raise AttributeError("id_tbl does not have an index_var attribute")
         return self._obj.index.names[1]
 
+    @property
+    def interval(self) -> str:
+        """the interval of the time index"""
+        if self.is_id_tbl():
+            raise AttributeError("id_tbl does not have an interval attribute")
+        return self._obj.index.dtypes[1]
+
     def set_index_var(self, index_var: str, drop: bool = False, inplace: bool = False) -> pd.DataFrame:
+        """Set a time index for the table
+
+        Args:
+            index_var: name of the column that should be used as time index
+            drop: whether any existing time index should dropped (True) or turned into a column (False). Defaults to False.
+            inplace: whether to modify the DataFrame rather than creating a new one. Defaults to False.
+
+        Returns:
+            table with time index
+        """
         self._validate()
         if not index_var in self._obj.columns:
             if index_var in self._obj.index.names:
                 warnings.warn(f"{index_var} is already part of the metadata, left unchanged")
+                return self._obj
             else: 
                 raise ValueError(f"tried to set Index to unknown column {index_var}")
         if not isinstance(self._obj[index_var].dtype, TimeDtype):
@@ -119,24 +201,51 @@ class TableAccessor:
     
     @property
     def time_vars(self):
+        """List of all time variables among table columns"""
         # TODO: add the time index
         return [c for c in self._obj.columns if isinstance(self._obj[c].dtype, TimeDtype)]
 
     def rename_all(self, mapper: Dict, inplace: bool = False) -> pd.DataFrame:
+        """Rename both columns and index names 
+
+        Args:
+            mapper: a mapping dictionary as accepted by pandas.DataFrame.rename
+            inplace: whether to modify the DataFrame rather than creating a new one. Defaults to False.
+
+        Returns:
+            renamed table
+        """
         new_obj = self._obj
-        new_obj = self._obj.rename(columns=mapper, inplace=inplace, errors="ignore")
-        new_obj = self._obj.rename_axis(index=mapper, inplace=inplace)
+        new_obj = new_obj.rename(columns=mapper, inplace=inplace, errors="ignore")
+        new_obj = new_obj.rename_axis(index=mapper, inplace=inplace)
         return new_obj
 
-    def change_interval(self, interval: TimeDtype, inplace: bool = False) -> pd.DataFrame:
+    def change_interval(self, interval: TimeDtype, cols: str | List[str] | None = None, inplace: bool = False) -> pd.DataFrame:
+        """Change the time interval of time columns
+
+        Args:
+            interval: new time interval
+            cols: time variables for which to change the interval. If None, change interval for 
+                all time variables, including those in the pandas index. Defaults to None.
+            inplace: whether to modify the DataFrame rather than creating a new one. Defaults to False.
+
+        Returns:
+            table with changed time interval
+        """
+        self._validate()
+        
         index_var = None
         new_obj = self._obj
-        
+        cols = enlist(cols)
+
         if self.is_ts_tbl():
             index_var = self.index_var
             new_obj = new_obj.reset_index(index_var, inplace=inplace)
+
+        if cols is None:
+            cols = new_obj.tbl.time_vars
             
-        for col in new_obj.tbl.time_vars:
+        for col in cols:
             new_obj[col] = new_obj[col].astype(interval, copy=not inplace)
         
         if index_var is not None:
@@ -221,37 +330,50 @@ class TableAccessor:
     def _upgrade_id_id_tbl(self, src: Src, target_id: str, cols: str | List[str] | None = None, **kwargs):
         return self._change_id_helper(src, target_id, cols, "up", **kwargs)
 
-    def _upgrade_id_ts_tbl(self, src: Src, target_id, cols, **kwargs):
+    def _upgrade_id_ts_tbl(self, src: Src, target_id, cols, id_type=False, **kwargs):
         if self.index_var not in cols:
             raise ValueError(f"index var `{self.index_var}` must be part of the cols parameter")
 
-        # if self.interval != minutes(1):
-        #     warnings.warn("Changing the ID of non-minute resolution data will change the interval to 1 minute")
+        x = self._obj
 
-        sft = new_names(self)
-        id = self.id_var
-        ind = self.index_var
+        sft = new_names(x)
+        id = x.tbl.id_var
+        ind = x.tbl.index_var
+        interval = x.tbl.interval
 
-        map = src.id_map(id, target_id, sft, ind)
+        if id_type:
+            id_nms = src.id_cfg.map_type_to_id()
+            map = src.id_map(id_nms[id], id_nms[target_id], sft, ind)
+            map = map.tbl.rename_all(src.id_cfg.map_id_to_type())
+        else:
+            map = src.id_map(id, target_id, sft, ind)
+        map = map.tbl.set_index_var(x.tbl.index_var)
+        map = map.tbl.change_interval(interval)
 
         # TODO: pandas currently does not have a direct equivalent to R data.table's rolling join
-        #       It can be approximated with pandas.merge_asof but needs additional sorting and
-        #       does not propagate rolls outside of ends (see data.table's `rollends` parameter).
-        #       this code may be slow and may need revisiting/refactoring.
-        tbl = self._obj.sort_values(ind)
-        map = map.sort_values(ind)
-        fwd = pd.merge_asof(tbl, map, on=ind, by=id, direction="forward")
-        not_matched = fwd[fwd[target_id].isna()][tbl.columns]
-        bwd = pd.merge_asof(not_matched, map, on=ind, by=id, direction="backward")
-        res = pd.concat((fwd[~fwd[target_id].isna()], bwd), axis=0)
-        res = res.sort_values([target_id, ind])
+        #       determine match groups ourself (maybe move into function if needed more often).
+        a = pd.DataFrame({'which': 0}, index=x.index)
+        b = pd.DataFrame({'which': 1}, index=map.index)
 
+        c = pd.concat((a, b))
+        c = c.sort_index(ascending=False)
+        c['group'] = c.groupby(level=0).which.cumsum().clip(lower=1)
+        c = c[c.which == 0]
+
+        x['group'] = c.loc[x.index, 'group']
+        
+        b = b.sort_index(ascending=False)
+        b['group'] = b.groupby(level=0).which.cumsum() # TODO: this is currently wrong
+        map['group'] = b.loc[map.index, 'group']
+
+        x = x.reset_index()
+        x = x.merge(map, on=[id, 'group'])
         for c in cols:
-            res[c] = res[c] - res[sft]
+            x[c] = x[c] - x[sft]
+        x = x.tbl.as_ts_tbl(target_id, ind)
+        x = x.drop(columns=[sft, 'group'])
 
-        res.drop(columns=sft, inplace=True)
-        #res = TsTbl(res, id_var=target_id, index_var=ind, interval=mins(1))
-        return res
+        return x
 
     def downgrade_id(self, src: Src, target_id: str, cols: str | List[str] | None = None, **kwargs):
         if cols is None:
---
+++ tests/test_accessor.py
@@ -0,0 +1,150 @@
+import pytest
+import numpy as np
+from pyicu.container.time import hours, minutes
+
+@pytest.fixture
+def id_tbl(example_df):
+    return example_df.set_index(['id_var'])
+
+@pytest.fixture
+def ts_tbl(example_df):
+    return example_df.set_index(['id_var', 'index_var'])
+
+@pytest.fixture
+def win_tbl(example_df):
+    return example_df.set_index(['id_var', 'index_var', 'win_var'])
+
+def test_validate(id_tbl, ts_tbl, win_tbl):
+    id_tbl.tbl._validate()
+    ts_tbl.tbl._validate()
+    win_tbl.tbl._validate()
+
+def test_validate_errors(example_df):
+    with pytest.raises(AttributeError) as e_info:
+        example_df.tbl._validate()
+    assert e_info.match("table must have named index.*")
+    with pytest.raises(AttributeError) as e_info:
+        example_df.set_index(['index_var', 'id_var']).tbl._validate()
+    assert e_info.match(".*the second must be a time index.*")
+    with pytest.raises(AttributeError) as e_info:
+        example_df.set_index(['index_var', 'id_var', 'win_var']).tbl._validate()
+    assert e_info.match(".*the second and third must be a time index.*")
+    with pytest.raises(AttributeError) as e_info:
+        example_df.set_index(['id_var', 'index_var', 'win_var', 'val_var']).tbl._validate()
+    assert e_info.match("only .* two or three levels are supported")
+    with pytest.raises(AttributeError) as e_info:
+        example_df.set_index(['index_var']).tbl._validate()
+    assert e_info.match("must have at least one non-time index")
+    
+    example_df.set_index(['id_var', 'index_var']).tbl._validate()
+    example_df.set_index(['id_var', 'index_var', 'win_var']).tbl._validate()
+
+def test_is_id_tbl(example_df, id_tbl, ts_tbl, win_tbl):
+    assert example_df.tbl.is_id_tbl() == False
+    assert id_tbl.tbl.is_id_tbl() == True
+    assert ts_tbl.tbl.is_id_tbl() == False
+    assert win_tbl.tbl.is_id_tbl() == False
+
+def test_set_id(example_df):
+    with pytest.warns(Warning) as w_info:
+        example_df.tbl.set_id_var('id_var').tbl.set_id_var('id_var')
+    with pytest.raises(ValueError) as e_info:
+        example_df.tbl.set_id_var('unknown_var')
+    assert e_info.match("tried to set Id to unknown column")
+
+def test_pandas_as_id_tbl(example_df):
+    id_tbl1 = example_df.tbl.as_id_tbl(id_var='id_var')
+    assert id_tbl1.tbl.id_var == "id_var"
+    id_tbl2 = example_df.tbl.as_id_tbl()
+    assert id_tbl2.tbl.id_var == "id_var"
+    id_tbl3 = example_df[['index_var', 'id_var']].tbl.as_id_tbl(id_var='id_var')
+    assert id_tbl3.tbl.id_var == "id_var"
+    with pytest.raises(TypeError) as e_info:
+        example_df[['index_var']].tbl.as_id_tbl()
+    assert e_info.match(".*no suitable non-time column could be found.*")
+
+def test_id_tbl_as_id_tbl(id_tbl):
+    id_tbl1 = id_tbl.tbl.as_id_tbl(id_var='id_var')
+    assert id_tbl1.tbl.id_var == "id_var"
+    id_tbl2 = id_tbl.tbl.as_id_tbl(id_var='val_var')
+    assert id_tbl2.tbl.id_var == "val_var"
+
+def test_ts_tbl_as_id_tbl(ts_tbl):
+    id_tbl1 = ts_tbl.tbl.as_id_tbl(id_var='val_var')
+    assert id_tbl1.tbl.is_id_tbl()
+    assert id_tbl1.tbl.id_var == 'val_var'
+    assert all([i in id_tbl1.columns for i in ts_tbl.index.names])
+    id_tbl2 = ts_tbl.tbl.as_id_tbl()
+    assert id_tbl2.tbl.is_id_tbl()
+    assert id_tbl2.tbl.id_var == 'id_var'
+
+def test_is_ts_tbl(example_df, id_tbl, ts_tbl, win_tbl):
+    assert example_df.tbl.is_ts_tbl() == False
+    assert id_tbl.tbl.is_ts_tbl() == False
+    assert ts_tbl.tbl.is_ts_tbl() == True
+    assert win_tbl.tbl.is_ts_tbl() == False
+
+def test_index_var(id_tbl, ts_tbl):
+    with pytest.raises(AttributeError) as e_info:
+        id_tbl.tbl.index_var
+    assert e_info.match("id_tbl does not have an index_var attribute")
+    assert ts_tbl.tbl.index_var == "index_var"
+
+def test_interval(id_tbl, ts_tbl):
+    with pytest.raises(AttributeError) as e_info:
+        id_tbl.tbl.interval
+    assert e_info.match("id_tbl does not have an interval attribute")
+    assert ts_tbl.tbl.interval == hours(1)
+
+def test_set_index(id_tbl):
+    with pytest.warns(Warning) as w_info:
+        id_tbl.tbl.set_index_var('index_var').tbl.set_index_var('index_var')
+    with pytest.raises(ValueError) as e_info:
+        id_tbl.tbl.set_index_var('unknown_var')
+    assert e_info.match("tried to set Index to unknown column.*")
+    with pytest.raises(TypeError) as e_info:
+        id_tbl.tbl.set_index_var('val_var')
+    assert e_info.match("index var must be TimeDtype.*")
+
+def test_pandas_as_ts_tbl(example_df):
+    ts_tbl1 = example_df.tbl.as_ts_tbl(id_var='id_var', index_var='index_var')
+    assert ts_tbl1.tbl.id_var == "id_var" and ts_tbl1.tbl.index_var == "index_var"
+    ts_tbl2 = example_df.tbl.as_ts_tbl(id_var='id_var')
+    assert ts_tbl2.tbl.id_var == "id_var" and ts_tbl2.tbl.index_var == "index_var"
+    ts_tbl3 = example_df[['id_var', 'val_var', 'index_var']].tbl.as_ts_tbl(id_var='id_var')
+    assert ts_tbl3.tbl.id_var == "id_var" and ts_tbl3.tbl.index_var == "index_var"
+    with pytest.raises(TypeError) as e_info:
+        example_df[['id_var', 'val_var']].tbl.as_ts_tbl()
+    assert e_info.match(".*no suitable time column could be found.*")
+
+def test_id_tbl_as_ts_tbl(id_tbl):
+    ts_tbl1 = id_tbl.tbl.as_ts_tbl(id_var='val_var', index_var='index_var')
+    assert ts_tbl1.tbl.id_var == "val_var" and ts_tbl1.tbl.index_var == "index_var"
+
+def test_ts_tbl_as_ts_tbl(ts_tbl):
+    ts_tbl1 = ts_tbl.tbl.as_ts_tbl()
+    assert ts_tbl1.tbl.id_var == "id_var" and ts_tbl1.tbl.index_var == "index_var"
+    ts_tbl2 = ts_tbl.tbl.as_ts_tbl(id_var='val_var')
+    assert ts_tbl2.tbl.id_var == "val_var" and ts_tbl2.tbl.index_var == "index_var"
+    ts_tbl3 = ts_tbl.tbl.as_ts_tbl(id_var='val_var', index_var='win_var')
+    assert ts_tbl3.tbl.id_var == "val_var" and ts_tbl3.tbl.index_var == "win_var"
+
+def test_rename_all(ts_tbl):
+    renamed = ts_tbl.tbl.rename_all({'id_var': "id", "val_var": "val"})
+    assert renamed.tbl.id_var == "id"
+    assert "val" in renamed.columns
+
+def test_change_interval_id_tbl(id_tbl):
+    changed = id_tbl.tbl.change_interval(minutes(5))
+    assert changed.index_var.dtype == minutes(5)
+    assert changed.win_var.dtype == minutes(5)
+    assert np.all(changed.index_var.values == np.array([0., 12., 12., 24., 0.]))
+
+def test_change_interval_ts_tbl(ts_tbl):
+    changed = ts_tbl.tbl.change_interval(minutes(5))
+    assert changed.index.levels[1].dtype == minutes(5)
+    assert changed.win_var.dtype == minutes(5)
+    assert np.all(changed.index.get_level_values(1) == np.array([0., 12., 12., 24., 0.]))
+    changed = ts_tbl.tbl.change_interval(minutes(5), cols='index_var')
+    assert changed.index.levels[1].dtype == minutes(5)
+    assert np.all(changed.index.get_level_values(1) == np.array([0., 12., 12., 24., 0.]))
