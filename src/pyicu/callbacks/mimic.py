d6e66fd86d24569fae23528c8810b56afef2a31c
prockenschaub <rockenschaub.patrick@gmail.com>
2022-12-21 16:19:28 +0100

    implement medication duration for mimic

--- src/pyicu/callbacks/mimic.py
+++ src/pyicu/callbacks/mimic.py
@@ -1,12 +1,17 @@
 import numpy as np
 import pandas as pd
 
+from ..utils import expand, expand_intervals
 from ..interval import days, minutes
 from .item import calc_dur
 
 def mimic_age(x: pd.Series, decimals=2) -> pd.Series:
     return np.round(-x.tm.change_interval(days(1)).astype(int) / 365.25, decimals)
 
+
+
+# ------------------------------------------------------------------------------
+# Medication concepts
 
 def mimic_abx_presc(x, val_var, **kwargs):
     idx = x.icu.index_var
@@ -16,13 +21,20 @@ def mimic_abx_presc(x, val_var, **kwargs):
     x[val_var] = True
     return x
 
-
-def mimic_dur_incv(x: pd.DataFrame, val_var: str, grp_var: str, **kwargs):
+def mimic_dur_incv(x: pd.DataFrame, val_var: str, grp_var: str, **kwargs) -> pd.DataFrame:
     """Calculate duration of medications in carevue"""
     ind = x.icu.index_var
     return calc_dur(x, val_var, ind, ind, grp_var)
 
-def mimic_dur_inmv(x: pd.DataFrame, val_var: str, grp_var: str, stop_var: str, **kwargs):
+def mimic_dur_inmv(x: pd.DataFrame, val_var: str, grp_var: str, stop_var: str, **kwargs) -> pd.DataFrame:
     """Calculate duration of medications in metavision"""
     ind = x.icu.index_var
     return calc_dur(x, val_var, ind, stop_var, grp_var)
+
+def mimic_rate_cv(x: pd.DataFrame, val_var: str, grp_var: str, unit_var: str, **kwargs) -> pd.DataFrame:
+    return expand_intervals(x, [val_var, unit_var], grp_var)
+
+def mimic_rate_mv(x: pd.DataFrame, val_var: str, unit_var: str, stop_var: str, **kwargs) -> pd.DataFrame:
+    id = x.icu.id_var
+    ind = x.icu.index_var
+    return expand(x, ind, stop_var, keep_vars=[id, val_var, unit_var])
--- src/pyicu/utils.py
+++ src/pyicu/utils.py
@@ -4,6 +4,7 @@ import pandas as pd
 import string
 import random
 
+from .interval import hours
 
 def enlist(x: Any):
     # TODO: Test for scalar instead
@@ -111,37 +112,128 @@ def expand(
     x: pd.DataFrame,
     start_var: str | None = None,
     end_var: str | None = None,
-    step_size: pd.Timedelta = None,
+    step_size: pd.Timedelta | None = None,
     new_index: str | None = None,
-    keep_vars: List[str] | None = None,
+    keep_vars: str | List[str] | None = None,
     aggregate: bool | str | Callable = False,
 ) -> pd.DataFrame:
-    if x.icu.is_pandas() or x.icu.is_id_tbl() or x.icu.is_ts_tbl():
+    """Expand table to one row per `step_size` from `start_var` to `end_var`
+
+    Note: An aspect to keep in mind when applying expand() to a win_tbl object 
+        is that values simply are repeated for all time-steps that fall into a 
+        given validity interval. This gives correct results when a win_tbl for 
+        example contains data on infusions as rates, but might not lead to 
+        correct results when infusions are represented as drug amounts 
+        administered over a given time-span. In such a scenario it might be 
+        desirable to evenly distribute the total amount over the corresponding 
+        time steps (currently not implemented).
+
+    Args:
+        x: ts_tbl or win_tbl
+        start_var: name of the column containing the start time. If None, use index variable if exists. Defaults to None.
+        end_var: name of the column containing the end time. Must be specified for ts_tbl. If win_tbl and None, use duration variable. Defaults to None.
+        step_size: size of the time steps to expand from start to end. If None, infer base interval of `x`. Defaults to None.
+        new_index: TODO: add this feature
+        keep_vars: which variables to retain in the output (id and index are always retained). Defaults to None.
+        aggregate: whether and how rows with the same id and index should be aggregated. Defaults to False.
+
+    Returns:
+        ts_tbl
+    """
+    if x.icu.is_pandas() or x.icu.is_id_tbl():
         # TODO: also handle these
         raise NotImplementedError()
+
+    if x.icu.is_ts_tbl() and end_var is None: 
+        raise ValueError(f'`end_var` must be specified to expand a ts_tbl')
 
     if start_var is None:
         start_var = x.icu.index_var
     if end_var is None:
         end_var = new_names(x)
+    if step_size is None: 
+        step_size = x.icu.interval
 
-    if x.icu.is_win_tbl():
+    if x.icu.is_ts_tbl():
+        x = x.reset_index(level=1)
+    elif x.icu.is_win_tbl():
         dur_var = x.icu.dur_var
         x = x.reset_index(level=[1, 2])
-
-        if end_var not in x.columns:
-            x[end_var] = x[start_var] + x[dur_var]
-            # TODO: deal with negative dur_var by setting it to zero
+        x[end_var] = x[start_var] + x[dur_var]
+        # TODO: deal with negative dur_var by setting it to zero
 
     steps = new_names(x)
     x[steps] = x.apply(lambda row: pd.timedelta_range(row[start_var], row[end_var], freq=step_size), axis=1)
     x = x.explode(steps)
 
-    x.drop(columns=[start_var, end_var, dur_var], inplace=True)
+    x.drop(columns=list(set(x.columns) - set(enlist(steps) + enlist(keep_vars))), inplace=True)
     x.rename(columns={steps: start_var}, inplace=True)
     x.set_index(start_var, append=True, inplace=True)
 
     if aggregate != False:
         x = x.groupby(level=[0, 1]).aggregate(aggregate)
 
+    return x
+
+def create_intervals(
+    x: pd.DataFrame, 
+    by_vars: str | List[str] | None = None,
+    overhang: pd.Timedelta = hours(1),
+    max_len: pd.Timedelta = hours(6),
+    dur_var: str = "duration"
+) -> pd.DataFrame:
+    """Creat time intervals between two subsequent measurements of the same group
+
+    Note: this function differs slightly from the original function in ricu. 
+          Some of the behaviour in ricu was obscure and not well documented, 
+          so this alternative implementation was chosen. 
+          TODO: open issue in ricu Github repo
+
+    Args:
+        x: ts_tbl
+        by_vars: variables to group measurements by. If None, measurements are grouped by id, e.g., icustay. 
+            Defaults to None.  
+        overhang: value for the last time interval (which is undefined). Defaults to hours(1).
+        max_len: maximum interval length. Defaults to hours(6).
+        dur_var: name of the new duration column. Defaults to "duration".
+
+    Returns:
+        win_tbl
+    """
+    if not x.icu.is_ts_tbl():
+        raise TypeError(f'expected `x` to be ts_tbl')
+
+    if by_vars is None: 
+        by_vars = x.icu.id_var
+    by_vars = enlist(by_vars)
+
+    id = x.icu.id_var
+    ind = x.icu.index_var
+    interval = x.icu.interval
+
+    x = x.reset_index(level=1)
+    x[dur_var] = x.groupby(id)[ind].shift(-1) - x[ind]
+    x[dur_var] = x[dur_var].fillna(overhang) - interval
+    x[dur_var] = x[dur_var].clip(lower=hours(0), upper=max_len-interval)
+
+    return x.icu.as_win_tbl(index_var=ind, dur_var=dur_var)
+
+def expand_intervals(x: pd.DataFrame, keep_vars: str | List[str] | None = None, grp_var: str | None = None) -> pd.DataFrame:
+    """Wrapper to first create a win_tbl using `create_intervals` and then `expand`
+
+    Args:
+        x: ts_tbl
+        keep_vars: which variables to retain in the output (id and index are always retained). Defaults to None.
+        grp_var: variables to group measurements by. If None, measurements are grouped by id, e.g., icustay. 
+            Defaults to None.  
+
+    Returns:
+        ts_tbl
+    """
+    id = x.icu.id_var
+    ind = x.icu.index_var
+    dur = 'duration'
+    grp = enlist(id) + enlist(grp_var)
+    x = create_intervals(x, grp, overhang=hours(1), max_len=hours(6), dur_var=dur)
+    x = expand(x, ind, dur, keep_vars=enlist(id)+enlist(keep_vars))
     return x
