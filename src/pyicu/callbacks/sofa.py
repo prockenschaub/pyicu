03756ab54169060a2b4ecc1c3ba5ef30c8218e32
Marco Schaarschmidt <m-schaarschmidt@outlook.de>
2023-05-30 09:53:15 +0200

    Add test for comparison of ricu and pyicu (rpy2)

--- src/pyicu/callbacks/sofa.py
+++ src/pyicu/callbacks/sofa.py
@@ -18,3 +18,69 @@ sofa_coag = sofa_single("plt", "sofa_coag", lambda x: 4 - pd.cut(x, [-np.inf, 20
 sofa_liver = sofa_single("bili", "sofa_liver", lambda x: pd.cut(x, [-np.inf, 1.2, 2, 6, 12, np.inf], labels=False, right=False))
 
 sofa_cns = sofa_single("gcs", "sofa_cns", lambda x: 4 - pd.cut(x, [-np.inf, 6, 10, 13, 15, np.inf], labels=False, right=False))
+
+#ToDo
+
+# Example from ricu in R
+'''
+sofa_cns <- sofa_single(
+  "gcs", "sofa_cns", function(x) 4L - findInterval(x, c(6, 10, 13, 15))
+)
+'''
+
+# sofa_cardio, without sofa_single in R
+
+'''
+sofa_cardio <- function(..., interval = NULL) {
+
+  score_calc <- function(map, dopa, norepi, dobu, epi) {
+    fifelse(
+      is_true(dopa > 15 | epi > 0.1 | norepi > 0.1), 4L, fifelse(
+        is_true(dopa > 5 | (epi > 0 &    epi <= 0.1) |
+                        (norepi > 0 & norepi <= 0.1)), 3L, fifelse(
+          is_true((dopa > 0 & dopa <= 5) | dobu > 0), 2L, fifelse(
+            is_true(map < 70), 1L, 0L
+          )
+        )
+      )
+    )
+  }
+
+  cnc <- c("map", "dopa60", "norepi60", "dobu60", "epi60")
+  dat <- collect_dots(cnc, interval, ..., merge_dat = TRUE)
+
+  dat <- dat[, c("sofa_cardio") := score_calc(
+    get("map"), get("dopa60"), get("norepi60"), get("dobu60"), get("epi60")
+  )]
+  dat <- rm_cols(dat, cnc, by_ref = TRUE)
+
+  dat
+}
+'''
+
+# sofa_renal, withoputn sofa_single in R
+
+'''
+sofa_renal <- function(..., interval = NULL) {
+
+  score_calc <- function(cre, uri) {
+    fifelse(
+      is_true(cre >= 5 | uri < 200), 4L, fifelse(
+        is_true((cre >= 3.5 & cre < 5) | uri < 500), 3L, fifelse(
+          is_true(cre >= 2 & cre < 3.5), 2L, fifelse(
+            is_true(cre >= 1.2 & cre < 2), 1L, 0L
+          )
+        )
+      )
+    )
+  }
+
+  cnc <- c("crea", "urine24")
+  dat <- collect_dots(cnc, interval, ..., merge_dat = TRUE)
+
+  dat <- dat[, c("sofa_renal") := score_calc(get("crea"), get("urine24"))]
+  dat <- rm_cols(dat, cnc, by_ref = TRUE)
+
+  dat
+}
+'''
---
+++ tests/rpy2-test.py
@@ -0,0 +1,69 @@
+### Test rpy2 to compare code/fucntions in "pyicu" and "ricu"
+import os
+import subprocess
+from pathlib import Path
+from contextlib import contextmanager
+import rpy2
+import sys
+
+import pyicu
+from pyicu.configs.load import load_src_cfg
+from pyicu.sources import MIMIC
+from pyicu.concepts import ConceptDict
+
+# import R's "base" and "utils" package
+from rpy2.robjects.packages import importr
+base = importr('base')
+utils = importr('utils')
+utils.chooseCRANmirror(ind=1)
+
+# Install "ricu" package from GitHub, using "remotes" library
+utils.install_packages('remotes')
+remotes = importr('remotes')
+remotes.install_github("eth-mds/ricu")
+ricu = importr('ricu')
+
+# Install mimic_demo dataset for ricu
+utils.install_packages("mimic.demo", repos = "https://eth-mds.github.io/physionet-demo")
+
+# Load "hr" concept from ricu
+hr_ricu = ricu.load_concepts("hr", "mimic_demo", verbose = rpy2.robjects.vectors.BoolVector([False]))
+
+# Download mimic demo dataset for pyicu
+@contextmanager
+def directory(path):
+    # Code copied from https://stackoverflow.com/questions/299446/how-do-i-change-directory-back-to-my-original-working-directory-with-python
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+url = "https://physionet.org/files/mimiciii-demo/1.4/"
+
+download_dir = Path("examples/data")
+download_dir.mkdir(parents=True, exist_ok=True)
+
+with directory(download_dir):
+    subprocess.run(["wget", "-r", "-N", "-c", "-np", url])
+
+# Load "hr" concept from pyicu
+if pyicu.__file__.endswith("pyicu/src/pyicu/__init__.py"):
+    sys.path.append(pyicu.__file__[:-22])
+    
+data_dir = Path("examples/data/physionet.org/files/mimiciii-demo/1.4")
+mimic_cfg = load_src_cfg("mimic_demo")
+mimic_cfg.do_import(data_dir)
+print(mimic_cfg)
+mimic_demo = MIMIC(mimic_cfg, data_dir)
+print(mimic_demo.print_available())
+
+concepts = ConceptDict.from_defaults()
+print("Concept: ", concepts['hr'])
+print("Items:   ", concepts['hr'].src_items(mimic_demo))
+hr_pyicu = concepts.load_concepts("hr", mimic_demo)
+
+# Print results from ricu and pyicu
+print(hr_ricu)
+print(hr_pyicu)
